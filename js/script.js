6f33b0978eb6ce1aa8e0779a7dc051600120211e
Your Name <you@example.com>
2025-03-17 19:30:47 +0100

    presentacion de la consigna

---
+++ js/script.js
@@ -0,0 +1,4 @@
+//nombre, imagen, tipo, altura y peso.
+// endpoint fetch(`https://pokeapi.co/api/v2/pokemon`)
+//Consejo: Usa el "value" del selector para poder sacar el valor del pokemon que queremos que nos de la información. 
+//`document.getElementById('pokemon-select').value` 
